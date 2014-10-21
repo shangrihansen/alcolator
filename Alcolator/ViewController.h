8f95de9eb36f10bf3b27662fd158d474fcc225ec
Colt Hansen <shangrihansen@gmail.com>
2014-10-20 21:08:54 -0700

    Initial Commit

---
+++ Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Colt Hansen on 10/20/14.
+//  Copyright (c) 2014 Colt Hansen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Alcolator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Alcolator
+//
+//  Created by Colt Hansen on 10/20/14.
+//  Copyright (c) 2014 Colt Hansen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
